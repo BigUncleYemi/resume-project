20abe39ba1f64dd92d6a026c99e69b7dea06aca5
Olayemi Oyebanji <olayemioyebanji911@gmail.com>
2021-08-21 20:44:15 +0100

    Task 2: my name printed in a lang of choice (Golang)

---
+++ myname.go
@@ -0,0 +1,21 @@
+// Including the main package
+package main
+  
+// Importing fmt
+import (
+    "fmt"
+)
+  
+// Calling main
+func main() {
+  
+    // Declaring some const variables
+    const name = "Oyebanji Olayemi"
+  
+    // Calling print() function
+    fmt.Print("My name is ", name);
+  
+    // It is conventional not to worry about any
+    // error returned by Print.
+  
+}
